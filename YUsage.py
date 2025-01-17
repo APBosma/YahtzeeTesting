49dc737dc06308eef1b04ad753375dc84615572c
Emilio Bosma <118491727+APBosma@users.noreply.github.com>
2025-01-17 10:07:17 -0500

    Update YUsage.py

--- YUsage.py
+++ YUsage.py
@@ -5,4 +5,7 @@ print("First roll:", yahtzee.roll_dice())
 print("Second roll after rerolling first and third dice:", yahtzee.reroll([0, 2]))
 print("Final roll:", yahtzee.get_roll())
 #print("Highest current possible score: ", yahtzee.score(yahtzee.get_roll()))
-yahtzee.score(yahtzee.get_roll())
+upperHS, lowerHS = yahtzee.score(yahtzee.get_roll())
+print("Highest possible upper score:", upperHS)
+print("Highest possible lower score:", lowerHS)
+
