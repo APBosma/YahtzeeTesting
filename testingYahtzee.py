d259cff2ce3a3eecb4b4506aa86681792d030873
Emilio Bosma <118491727+APBosma@users.noreply.github.com>
2025-01-17 10:06:32 -0500

    Update testingYahtzee.py

--- testingYahtzee.py
+++ testingYahtzee.py
@@ -9,52 +9,75 @@ class TestYahtzee(unittest.TestCase):
     def testCounts(self):
         roll = [2, 2, 3, 3, 5]
         upper,lower = self.rolls.score(roll)
-        self.assertEqual(self.rolls.score(roll), "Highest possible upper score: 6\nHighest possible lower score: 0")
-    '''    
+        self.assertEqual(lower, 0)
+        self.assertEqual(upper, 6)
+
+        
     def testsecondCount(self):
         roll = [5, 5, 3, 1, 1]
-        self.assertEqual(self.rolls.score(roll),15)
+        upper,lower = self.rolls.score(roll)
+        self.assertEqual(lower, 0)
+        self.assertEqual(upper, 10)
         
     #testing three and four
     def testthreeTest(self):
         roll = [5, 5, 5, 2, 1]
-        self.assertEqual(self.rolls.score(roll), 18)
+        upper,lower = self.rolls.score(roll)
+        self.assertEqual(lower, 18)
+        self.assertEqual(upper, 15)
         
     def testfourTest(self):
         roll = [6, 6, 6, 6, 5]
-        self.assertEqual(self.rolls.score(roll), 29)
+        upper,lower = self.rolls.score(roll)
+        self.assertEqual(lower, 29)
+        self.assertEqual(upper, 24)
         
     #small straight
     def testsmallStraightTest(self):
         roll = [1, 2, 3, 4, 1]
-        self.assertEqual(self.rolls.score(roll), 30)
+        upper,lower = self.rolls.score(roll)
+        self.assertEqual(lower, 30)
+        self.assertEqual(upper, 4)
     
     def testsmallStraightTest2(self):
         roll = [2, 3, 4, 5, 5]
-        self.assertEqual(self.rolls.score(roll), 30)
+        upper,lower = self.rolls.score(roll)
+        self.assertEqual(lower, 30)
+        self.assertEqual(upper, 10)
         
     #large straight
     def testlargeStraightTest(self):
         roll = [2, 3, 4, 5, 6]
-        self.assertEqual(self.rolls.score(roll), 40)
+        upper,lower = self.rolls.score(roll)
+        self.assertEqual(lower, 40)
+        self.assertEqual(upper, 6)
     
     def testlargeStraightTest2(self):
         roll = [2, 3, 4, 5, 1]
-        self.assertEqual(self.rolls.score(roll), 40)
+        upper,lower = self.rolls.score(roll)
+        self.assertEqual(lower, 40)
+        self.assertEqual(upper, 5)
         
     #house
     def testHouse(self):
         roll = [2, 2, 2, 4, 4]
-        self.assertEqual(self.rolls.score(roll), 25)
+        upper,lower = self.rolls.score(roll)
+        self.assertEqual(lower, 25)
+        self.assertEqual(upper, 8)
         
     def testHouse2(self):
         roll = [1, 1, 1, 3, 3]
-        self.assertEqual(self.rolls.score(roll), 25)
+        upper,lower = self.rolls.score(roll)
+        self.assertEqual(lower, 25)
+        self.assertEqual(upper, 6)
         
+    
     def testyahtzeeTest(self):
         roll = [1, 1, 1, 1, 1]
-        self.assertEqual(self.rolls.score(roll), 50)
-      '''  
+        upper,lower = self.rolls.score(roll)
+        self.assertEqual(lower, 50)
+        self.assertEqual(upper, 5)
+      
     
 if __name__ == '__main__':
     unittest.main()
